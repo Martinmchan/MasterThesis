f663e6aca0675739b4b14b1e7f0ce425335c9ecd
Martin Chan <martinchan94@hotmail.com>
2018-01-18 10:27:01 +0100

    deleted all data

--- tdoaTest.m
+++ tdoaTest.m
@@ -1,26 +1,31 @@
 clear all
 close all
 
-[y1, Fs1] = audioread('000102_263_mono1.wav');
-[y3, Fs3] = audioread('000102_263_mono3.wav');
+[y1, Fs1] = audioread('000101_257_mono1.wav');
+[y3, Fs3] = audioread('000101_257_mono3.wav');
+
 
 [sz, ~] = size(y1);
-treshold = max(y1)/3;
-tresholdValue = find(y1 >= treshold);
+treshold = max(y3);
+tresholdValue = find(y3 >= treshold);
 
 
-start = tresholdValue(1) - floor(sz/30);
+start = tresholdValue(1) - floor(sz/200);
 finish = tresholdValue(1) + floor(sz/30);
 
 suby1 = y1(start:finish);
 suby3 = y3(start:finish);
+
+plot(suby3)
+hold on
+plot(suby1)
 
 x1 = fft(suby1);
 x3 = fft(suby3);
 
 L = finish-start;
 
-gPhat31 = x3.*conj(x1)/(x3'*conj(x1));
+gPhat31 = x3.*conj(x1)/norm((x3'*conj(x1)));
 
 r31 = ifft(gPhat31);
 
