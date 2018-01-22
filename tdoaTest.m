c0dfa86dff492ad9ef9534261a1be1707f302d1b
Martin Chan <martinchan94@hotmail.com>
2018-01-22 10:09:04 +0100

    functioning gccphat

--- tdoaTest.m
+++ tdoaTest.m
@@ -1,9 +1,8 @@
 clear all
 close all
 
-[y1, Fs1] = audioread('000102_270_mono1.wav');
-[y3, Fs3] = audioread('000102_270_mono3.wav');
-
+[y1, Fs1] = audioread('000103_240_mono1.wav');
+[y3, Fs3] = audioread('000103_240_mono2.wav');
 
 [sz, ~] = size(y1);
 treshold = max(y3);
@@ -15,24 +14,23 @@ finish = tresholdValue(1) + floor(sz/50);
 
 suby1 = y1(start:finish);
 suby3 = y3(start:finish);
-grid on
-plot(suby3)
-hold on
-plot(suby1)
 
-x1 = fft(suby1);
-x3 = fft(suby3);
+% plot(suby1)
+% hold on
+% plot(suby3)
 
 L = finish-start;
 
-gPhat31 = x3.*conj(x1)/norm((x3'*conj(x1)));
+Ncorr = 2*L-1; 
+NFFT = 2^nextpow2(Ncorr);
+R12 = bsxfun(@times,fft(suby3,NFFT),conj(fft(suby1,NFFT)));
+r12_temp = fftshift(ifft(exp(1i*angle(R12))),1);
+r12 = r12_temp(NFFT/2+1-(L-1)/2:NFFT/2+1+(L-1)/2,:);
 
-r31 = ifft(gPhat31);
-
-[~, argM31] = max(r31);
-
-tdoa31 = ((L+1)/2 - argM31)/Fs1
-
-deltaS = tdoa31*343
+lags = (-(Ncorr-1)/2:(Ncorr-1)/2).';
+lags = lags/Fs1;
+[~,idx] = max(abs(r12));
+tau = L/(2*Fs1)+lags(idx)
+deltaS = 343*tau
 
 
