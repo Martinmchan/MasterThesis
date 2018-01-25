f15551e19dfcdbb02752a2165c208d98a46ed7e7
Martin Chan <martinch@lnxmartinch.se.axis.com>
2018-01-25 13:52:40 +0100

    changed miniPlayCap

--- miniPlayCap.c
+++ miniPlayCap.c
@@ -4,56 +4,51 @@
 //.miniPlayCap audiosource localhw:0 test.wav
 
 
-main (int argc, char *argv[]){
-//Initialization for both play and record
-	char *buffer;
-	unsigned int pcm, tmp;
-	int buffer_frames = 128;
-	signed int rate = 48000;
-	int channels = 1;
-	int seconds = 8; 
-	snd_pcm_t *handle;
-	snd_pcm_hw_params_t *hw_params;
-	snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE;
+
+#include <alsa/asoundlib.h>
+#include <stdio.h>
+
+#define PCM_DEVICE "default"
+
+int main(int argc, char **argv) {
+	unsigned int pcm, tmp, dir;
+	int rate, channels, seconds;
+	snd_pcm_t *pcm_handle;
+	snd_pcm_hw_params_t *params;
 	snd_pcm_uframes_t frames;
+	char *buff;
 	int buff_size, loops;
+	rate 	 = 48000;
+	channels = 1;
+	seconds  = 2;
 
-//Prepare Speaker
-	snd_pcm_open(&handle, argv[2], SND_PCM_STREAM_PLAYBACK, 0);
-	snd_pcm_hw_params_alloca(&hw_params);
-	snd_pcm_hw_params_any(handle, hw_params);
-	snd_pcm_hw_params_set_access(handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED);
-	snd_pcm_hw_params_set_format(handle, hw_params, SND_PCM_FORMAT_S16_LE);
-	snd_pcm_hw_params_set_channels(handle, hw_params, channels);
-	snd_pcm_hw_params_set_rate_near(handle, hw_params, &rate, 0);
-	snd_pcm_hw_params(handle, hw_params);
+	snd_pcm_open(&pcm_handle, PCM_DEVICE, SND_PCM_STREAM_PLAYBACK, 0);
+	snd_pcm_hw_params_alloca(&params);
+	snd_pcm_hw_params_any(pcm_handle, params);
+	snd_pcm_hw_params_set_access(pcm_handle, params, SND_PCM_ACCESS_RW_INTERLEAVED);
+	snd_pcm_hw_params_set_format(pcm_handle, params, SND_PCM_FORMAT_S16_LE); 
+	snd_pcm_hw_params_set_channels(pcm_handle, params, channels);
+	snd_pcm_hw_params_set_rate_near(pcm_handle, params, &rate, 0); 
+	snd_pcm_hw_params(pcm_handle, params);
 
-	snd_pcm_hw_params_get_period_size(hw_params, &frames, 0);
+	snd_pcm_hw_params_get_channels(params, &tmp);
+
+	/* Allocate buffer to hold single period */
+	snd_pcm_hw_params_get_period_size(params, &frames, 0);
 
 	buff_size = frames * channels * 2;
-	buffer = (char *) malloc(buff_size);
+	buff = (char *) malloc(buff_size);
 
-	snd_pcm_hw_params_get_period_time(hw_params, &tmp, NULL);
+	snd_pcm_hw_params_get_period_time(params, &tmp, NULL);
 
 	for (loops = (seconds * 1000000) / tmp; loops > 0; loops--) {
-
-		if (pcm = read(0, buffer, buff_size) == 0) {
-			printf("Early end of file.\n");
-			return 0;
-		}
-
-		if (pcm = snd_pcm_writei(handle, buffer, frames) == -EPIPE) {
-			printf("XRUN.\n");
-			snd_pcm_prepare(handle);
-		} else if (pcm < 0) {
-			printf("ERROR. Can't write to PCM device. %s\n", snd_strerror(pcm));
-		}
-
+		read(0, buff, buff_size);
+		snd_pcm_writei(pcm_handle, buff, frames);
 	}
 
-	snd_pcm_drain(handle);
-	snd_pcm_close(handle);
-	free(buffer);
+	snd_pcm_drain(pcm_handle);
+	snd_pcm_close(pcm_handle);
+	free(buff);
 
 	return 0;
 }
