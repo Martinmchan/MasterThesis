ce1ccb2f8a484383854c23c5af3c490b67f74f3e
Martin Chan <martinch@lnxmartinch.se.axis.com>
2018-02-08 11:17:48 +0100

    simluated ourSync

---
+++ MATLAB/syncSimulation.m
@@ -0,0 +1,129 @@
+close all;
+clear all;
+
+f = 48000;
+reclength = f*16;
+
+off1 = rand()*0.01;
+off2 = rand()*0.01;
+off3 = rand()*0.01;
+
+noise1 = (rand(1,reclength)*2-1)*0.005 + off1;
+noise2 = (rand(1,reclength)*2-1)*0.005 + off2;
+noise3 = (rand(1,reclength)*2-1)*0.005 + off3;
+
+
+sFreq = 250;
+sLength = 1*f;
+xSignal = 1:sLength;
+signal = sin(sFreq/sLength*2*pi*xSignal);
+
+
+delay1 = floor(0*f);
+delay2 = floor(0.6*f);
+delay3 = floor(1*f);
+
+dist12 = 1.5;
+dist13 = 1.5;
+dist23 = 3;
+
+amp = 0.07;
+
+mic1 = zeros(1,reclength);
+mic1(2*f + 1:2*f + sLength) = amp*signal;
+mic1(6*f + floor(dist12/343*f) + 1:6*f + floor(dist12/343*f) + sLength) = amp*0.4*signal;
+mic1(10*f+ floor(dist13/343*f) + 1:10*f + floor(dist13/343*f) + sLength) = amp*0.4*signal;
+mic1 = mic1 + noise1;
+
+mic2 = zeros(1,reclength);
+mic2(2*f + floor(dist12/343*f) + 1:2*f + floor(dist12/343*f) + sLength) = amp*0.4*signal;
+mic2(6*f + 1:6*f + sLength) = amp*signal;
+mic2(10*f + floor(dist23/343*f) + 1:10*f + floor(dist23/343*f) + sLength) = amp*0.2*signal;
+mic2 = mic2 + noise2;
+
+mic3 = zeros(1,reclength);
+mic3(2*f + floor(dist13/343*f) + 1:2*f + floor(dist13/343*f) + sLength) = amp*0.4*signal;
+mic3(6*f + floor(dist23/343*f) + 1:6*f + floor(dist23/343*f) + sLength) = amp*0.2*signal;
+mic3(10*f+ 1:10*f + sLength) = amp*signal;
+mic3 = mic3 + noise3;
+
+
+if delay1 > 0
+    tmp = (rand(1,delay1)*2-1)*0.005 + off1;
+    mic1 = [tmp mic1];
+elseif delay1 < 0
+    mic1 = mic1(delay1:end);
+    tmp = (rand(1,delay1)*2-1)*0.005 + off1;
+    mic1 = [mic1 tmp];
+end
+
+
+if delay2 > 0
+    tmp = (rand(1,delay2)*2-1)*0.005 + off2;
+    mic2 = [tmp mic2];
+elseif delay2 < 0
+    mic2 = mic2(delay2:end);
+    tmp = (rand(1,delay2)*2-1)*0.005 + off2;
+    mic2 = [mic2 tmp];
+end
+
+
+
+if delay3 > 0
+    tmp = (rand(1,delay3)*2-1)*0.005 + off3;
+    mic3 = [tmp mic3];
+elseif delay3 < 0
+    mic3 = mic3(delay3:end);
+    tmp = (rand(1,delay3)*2-1)*0.005 + off3;
+    mic3 = [mic3 tmp];
+end
+
+plot(mic1);
+hold on
+plot(mic2);
+plot(mic3);
+figure
+
+mic1 = mic1';
+mic2 = mic2';
+mic3 = mic3';
+
+mic1 = mic1 - mean(mic1);
+mic2 = mic2 - mean(mic2);
+mic3 = mic3 - mean(mic3);
+
+s1 = 1:200000;
+s2 = 200001:400000;
+s3 = 400001:600000;
+
+mic2 = knownSync(mic1, mic2, s1, dist12);
+mic3 = knownSync(mic1, mic3, s1, dist13);
+
+plot(mic1)
+hold on
+plot(mic2)
+plot(mic3)
+
+
+tdoa = ourGccphat(mic1(s2),mic3(s2));
+deltaS = tdoa/f*343
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
