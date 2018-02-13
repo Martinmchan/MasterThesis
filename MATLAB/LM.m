2aca7ce596d0e62120c69a0b75021beb3097982a
Martin Chan <martinch@lnxmartinch.se.axis.com>
2018-02-13 15:37:42 +0100

    something

--- MATLAB/LM.m
+++ MATLAB/LM.m
@@ -1,12 +1,15 @@
 clear all
-x0 = [1.6,0.5,0]';
+x0 = [90,2,3]';
 
 lb = [0,0,0];
-ub = [2,2,0];
+ub = [2,2,2];
 
-xP = lsqnonlin(@myFunc,x0, lb, ub)
+options.Algorithm = 'levenberg-marquardt';
+
+
+xP = lsqnonlin(@myFunc,x0, lb,ub)
 
 param1 = xP;
-a1 = myFunc(param1)
+a1 = myFunc(param1);
 param2 = [0,0,0];
 a2 = myFunc(param2);
--- MATLAB/runSync.m
+++ MATLAB/runSync.m
@@ -1,9 +1,9 @@
 close all;
 clear all;
 
-[mic1, f] = audioread('000128_248_mono1.wav');
-[mic2, f] = audioread('000128_248_mono2.wav');
-[mic3, f] = audioread('000128_248_mono3.wav');
+[mic1, f] = audioread('cap1682nd.wav');
+[mic2, f] = audioread('cap992nd.wav');
+[mic3, f] = audioread('cap382nd.wav');
 
 size = length(mic1);
 
@@ -17,9 +17,9 @@ mic1 = mic1 - mean(mic1);
 mic2 = mic2 - mean(mic2);
 mic3 = mic3 - mean(mic3);
 
-s1 = 150000:300000;
-s2 = 300001:500000;
-s3 = 500001:700000;
+s1 = 1:200000;
+s2 = 200001:400000;
+s3 = 400001:600000;
 
 % [x1Start x1End] = findTone(mic1,mic2,mic3);
 % s2 = x1End:length(mic1);
@@ -43,15 +43,15 @@ s3 = 500001:700000;
 % plot(x1End + x2End + x3End,0.05,'o');
 % figure
 
-% mic2 = ourSync(mic1, mic2, s1, s2);
-% mic3 = ourSync(mic1, mic3, s1, s3);
-% 
-% 
-% plot(mic1)
-% hold on
-% plot(mic2)
-% plot(mic3)
-% 
+mic2 = ourSync(mic1, mic2, s1, s2);
+mic3 = ourSync(mic1, mic3, s1, s3);
+
+
+plot(mic1)
+hold on
+plot(mic2)
+plot(mic3)
+
 
 tdoa12 = ourGccphat(mic1(s3),mic2(s3));
 deltaS12 = abs(tdoa12/f*343);
