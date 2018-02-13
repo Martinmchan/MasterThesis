9aeba1cd7cd28f9a1036ac862dc2a729f072aa56
Sofie Karlsson <sofieka@lnxsofieka1.se.axis.com>
2018-02-13 16:11:37 +0100

    new LM

--- MATLAB/LM.m
+++ MATLAB/LM.m
@@ -1,15 +1,33 @@
 clear all
-x0 = [90,2,3]';
 
+%Read the files
+[y1, f] = audioread('000129_241_mono1.wav');
+[y2, f] = audioread('000129_241_mono2.wav');
+[y3, f] = audioread('000129_241_mono3.wav');
+[y4, f] = audioread('000129_241_mono4.wav');
+
+%Compute tdoa
+tdoa12 = ourGccphat(y1, y2)/f*343;
+tdoa13 = ourGccphat(y1, y3)/f*343;
+tdoa14 = ourGccphat(y1, y4)/f*343;
+
+%The microphones positions
+xyz = [0 0 0; 0 2 0; 1 2 0; 1 0 0];
+
+%Initial guess
+x0 = [1,1,1]';
+
+%Boundaries on the room
 lb = [0,0,0];
-ub = [2,2,2];
+ub = [1,2,2];
 
+%Calculate the position of the sound source using Levenberg-Marquardt
 options.Algorithm = 'levenberg-marquardt';
+xP = lsqnonlin(@myFunc,x0, lb,ub, [], tdoa12, tdoa13, tdoa14, xyz)
 
 
-xP = lsqnonlin(@myFunc,x0, lb,ub)
 
-param1 = xP;
-a1 = myFunc(param1);
-param2 = [0,0,0];
-a2 = myFunc(param2);
+% param1 = xP;
+% a1 = myFunc(param1);
+% param2 = [0,0,0];
+% a2 = myFunc(param2);
--- MATLAB/myFunc.m
+++ MATLAB/myFunc.m
@@ -1,25 +1,24 @@
-function outF = myFunc(param)
+function outF = myFunc(param, tdoa12, tdoa13, tdoa14, xyz)
 x = param(1);
 y = param(2);
 z = param(3);
 
-mic1 = [0,2,2];
-mic2 = [2,0,2];
-mic3 = [2,2,2];
-mic4 = [0,0,2];
-%mic5 = [1,2,2];
+% mic1 = [0,2,2];
+% mic2 = [2,0,2];
+% mic3 = [2,2,2];
+% mic4 = [0,0,2];
+% %mic5 = [1,2,2];
+% 
+% 
+% dist12 = norm(mic1) - norm(mic2);
+% dist13 = norm(mic1) - norm(mic3);
+% dist14 = norm(mic1) - norm(mic4);
+% %dist15 = norm(mic1) - norm(mic5);
 
-
-dist12 = norm(mic1) - norm(mic2);
-dist13 = norm(mic1) - norm(mic3);
-dist14 = norm(mic1) - norm(mic4);
-%dist15 = norm(mic1) - norm(mic5);
-
-func1 = dist12 - sqrt((x-mic1(1)).^2 + (y-mic1(2)).^2 + (z-mic1(3)).^2) + sqrt((x-mic2(1)).^2 + (y-mic2(2)).^2 + (z-mic2(3)).^2);
-func2 = dist13 - sqrt((x-mic1(1)).^2 + (y-mic1(2)).^2 + (z-mic1(3)).^2) + sqrt((x-mic3(1)).^2 + (y-mic3(2)).^2 + (z-mic3(3)).^2);
-func3 = dist14 - sqrt((x-mic1(1)).^2 + (y-mic1(2)).^2 + (z-mic1(3)).^2) + sqrt((x-mic4(1)).^2 + (y-mic4(2)).^2 + (z-mic4(3)).^2);
+func1 = tdoa12 - sqrt((x-xyz(1,1)).^2 + (y-xyz(1,2)).^2 + (z-xyz(1,3)).^2) + sqrt((x-xyz(2,1)).^2 + (y-xyz(2,2)).^2 + (z-xyz(2,3)).^2);
+func2 = tdoa13 - sqrt((x-xyz(1,1)).^2 + (y-xyz(1,2)).^2 + (z-xyz(1,3)).^2) + sqrt((x-xyz(3,1)).^2 + (y-xyz(3,2)).^2 + (z-xyz(3,3)).^2);
+func3 = tdoa14 - sqrt((x-xyz(1,1)).^2 + (y-xyz(1,2)).^2 + (z-xyz(1,3)).^2) + sqrt((x-xyz(4,1)).^2 + (y-xyz(4,2)).^2 + (z-xyz(4,3)).^2);
 %func4 = dist15 - sqrt((x-mic1(1)).^2 + (y-mic1(2)).^2 + (z-mic1(3)).^2) + sqrt((x-mic5(1)).^2 + (y-mic5(2)).^2 + (z-mic5(3)).^2);
-
 
 outF = [func1; func2; func3];
 
