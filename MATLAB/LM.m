69279776fbcca5aa1f97d3cf522485b71fe136ff
Martin Chan <martinch@lnxmartinch.se.axis.com>
2018-02-14 11:09:37 +0100

    removed tascam

--- MATLAB/LM.m
+++ MATLAB/LM.m
@@ -1,13 +1,14 @@
 clear all
 close all
 %Read the files
-[y1, f] = audioread('000129_244_mono1.wav');
-[y2, f] = audioread('000129_244_mono2.wav');
-[y3, f] = audioread('000129_244_mono3.wav');
-[y4, f] = audioread('000129_244_mono4.wav');
+[y1, f] = audioread('000129_242_mono1New.wav');
+[y2, f] = audioread('000129_242_mono2New.wav');
+[y3, f] = audioread('000129_242_mono3New.wav');
+[y4, f] = audioread('000129_242_mono4New.wav');
 
-plot(y1)
+plot(y2)
 hold on
+
 
 %Locate the sound in time
 [xStart xEnd] = findSound(y1);
@@ -17,32 +18,37 @@ y3 = y3(xStart:xEnd);
 y4 = y4(xStart:xEnd);
 
 
-plot(xStart,0,'go');
-plot(xEnd,0,'ro');
+plot(xStart,0.1,'go');
+plot(xEnd,0.1,'ro');
 
 
 %Compute tdoa
-tdoa12 = ourGccphat(y1, y2)/f*343;
-tdoa13 = ourGccphat(y1, y3)/f*343;
-tdoa14 = ourGccphat(y1, y4)/f*343;
+[tdoa12a, tdoa12b, tdoa12c] = ourGccphat(y1, y2);
+[tdoa13a, tdoa13b, tdoa13c] = ourGccphat(y1, y3);
+[tdoa14a, tdoa14b, tdoa14c] = ourGccphat(y1, y4);
+
+
+tdoa12a = tdoa12a/f*343;tdoa12b = tdoa12b/f*343;tdoa12c = tdoa12c/f*343;
+tdoa13a = tdoa13a/f*343;tdoa13b = tdoa13b/f*343;tdoa13c = tdoa13c/f*343;
+tdoa14a = tdoa14a/f*343;tdoa14b = tdoa14b/f*343;tdoa14c = tdoa14c/f*343;
 
 %The microphones positions
-xyz = [0 0 0; 0 2 0; 1 2 0; 1 0 0];
+xyz = [0 0 0; 0 1.35 0; 2.1 1.35 0; 2.1 0 0];
 
 %Initial guess
-x0 = [1,1,1]';
+x0 = [1.05,0.7,1]';
 
 %Boundaries on the room
-lb = [0,0,0];
-ub = [1,2,2];
+lb = [-0.1,-0.1,-0.1];
+ub = [3,2,2.1];
 
 %Calculate the position of the sound source using Levenberg-Marquardt
 options.Algorithm = 'levenberg-marquardt';
-xP = lsqnonlin(@myFunc,x0, lb,ub, [], tdoa12, tdoa13, tdoa14, xyz)
+xP = lsqnonlin(@myFunc,x0, lb,ub, [], tdoa12a, tdoa13a, tdoa14a, xyz)
 
 
 
 param1 = xP;
-a1 = myFunc(param1, tdoa12, tdoa13, tdoa14, xyz);
+a1 = myFunc(param1, tdoa12a, tdoa13a, tdoa14a, xyz);
 % param2 = [0,0,0];
 % a2 = myFunc(param2);
--- MATLAB/ourGccphat.m
+++ MATLAB/ourGccphat.m
@@ -1,19 +1,26 @@
-function tdoa = ourGccphat(mic1,mic2)
-
+function [tdoa1, tdoa2, tdoa3] = ourGccphat(mic1,mic2)
 
 %GCC-Phat
 N = length(mic1);
 Ncorr = 2*N-1; 
 NFFT = 2^nextpow2(Ncorr);
-func = @(x) 1./(abs(x)+(abs(x)<10e-3));
+func = @(x) 1./(abs(x)+(abs(x)<5e-3));
 R12 = bsxfun(@times,fft(mic1,NFFT),conj(fft(mic2,NFFT)));
 r12_temp = fftshift(ifft(R12.*func(R12)),1);
 r12 = r12_temp(floor(NFFT/2)+1-floor((N-1)/2):floor(NFFT/2)+1+floor((N-1)/2));
-
+% plot([-floor(length(r12)/2):floor(length(r12)/2)],r12);
 
 %Finding the TDOA and distance between microphones
 lags = (-(Ncorr-1)/2:(Ncorr-1)/2).';
 [~,idx] = max(abs(r12));
-tdoa = floor((N/2+lags(idx)));
+tdoa1 = floor((N/2+lags(idx)));
+r12(idx) = 0;
+
+[~,idx] = max(abs(r12));
+tdoa2 = floor((N/2+lags(idx)));
+r12(idx) = 0;
+
+[~,idx] = max(abs(r12));
+tdoa3 = floor((N/2+lags(idx)));
 
 end
