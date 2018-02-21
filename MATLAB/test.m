f14becee8e21ea0fd468abc26826396c3548dcc8
Martin Chan <martinch@lnxmartinch.se.axis.com>
2018-02-21 11:14:30 +0100

    pdf

--- MATLAB/test.m
+++ MATLAB/test.m
@@ -1,35 +1,71 @@
+close all;
+clear all;
+%Initialize camera position
+cameraMatrix = [0 0 1.7; -0.1 2.5 1.7; 2 0.5 1.65; 2.0 2.6 1.85];
 
+%Reads the data and plots them
+[mic1, f] = audioread('cap168.wav');
+[mic2, f] = audioread('cap99.wav');
+[mic3, f] = audioread('cap38.wav');
+[mic4, f] = audioread('cap250.wav');
 
-close all
-clear all
+mic1 = mic1 - mean(mic1);
+mic2 = mic2 - mean(mic2);
+mic3 = mic3 - mean(mic3);
+mic4 = mic4 - mean(mic4);
 
-Fs = 48000;
+plot(mic1)
+hold on
+plot(mic2)
+plot(mic3)
+plot(mic4)
 
+%Syncs all the microphones using mic1 as master, then plot the result.
+%Distance between the microphones are also shown as an indicator of how
+%good the sync was.
+s1 = 1:200000;
+s2 = 200001:400000;
+s3 = 400001:600000;
+s4 = 600001:800000;
 
-t = 1:48000;
-y = sin(3*2*pi*t/Fs);
-y = y + sin(5*2*pi*t/Fs);
-
-
-
-plot(y)
-
-Y = fft(y);
-
-L = length(Y);
-
-P1 = abs(Y);
-f = Fs*(0:L-1)/L;
-
-% plot(f,P1)
-
-Y(1:4) = 0;
-Y(L-3:L) = 0;
-
-y = ifft(Y);
+[mic2, dist12] = ourSync2(mic1, mic2, s1, s2);
+[mic3, dist13] = ourSync2(mic1, mic3, s1, s3);
+[mic4, dist14] = ourSync2(mic1, mic4, s1, s4);
 
 figure
-plot(real(y))
+plot(mic1)
+hold on
+plot(mic2)
+plot(mic3)
+plot(mic4)
 
-%sound(abs(y),48000)
+
+%%
+    L = 200000;
+    N = 12;
+    targetF = 4000;
+    f = 48000;
+    freq_index = round(targetF/f*N) + 1;
+
+    y1 = mic1(s3);
+    y2 = mic4(s3);
+    for i = 1:N:L-100
+        dft_data = abs(goertzel(y1(i:i+N),freq_index));
+        if dft_data >0.1
+            cap1= i;
+            break;
+        end
+    end
+
+    for i = 1:N:L-100
+        dft_data = abs(goertzel(y2(i:i+N),freq_index));
+        if dft_data >0.1
+            cap2= i;
+            break;
+        end
+    end
+
+    tdoa = (cap1-cap2)*343/f
+
+
 
