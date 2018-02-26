758bccf8e65838fa339cd57c3d36273ffd857d3a
Martin Chan <martinch@lnxmartinch.se.axis.com>
2018-02-26 10:38:43 +0100

    First sync

--- MATLAB/myFunc.m
+++ MATLAB/myFunc.m
@@ -1,4 +1,4 @@
-function outF = myFunc(param, tdoa12, tdoa13, tdoa14, tdoa15, xyz)
+function outF = myFunc(param, tdoa12, tdoa13, tdoa14, xyz)
 x = param(1);
 y = param(2);
 z = param(3);
@@ -6,9 +6,8 @@ z = param(3);
 func1 = tdoa12 - sqrt((x-xyz(1,1)).^2 + (y-xyz(1,2)).^2 + (z-xyz(1,3)).^2) + sqrt((x-xyz(2,1)).^2 + (y-xyz(2,2)).^2 + (z-xyz(2,3)).^2);
 func2 = tdoa13 - sqrt((x-xyz(1,1)).^2 + (y-xyz(1,2)).^2 + (z-xyz(1,3)).^2) + sqrt((x-xyz(3,1)).^2 + (y-xyz(3,2)).^2 + (z-xyz(3,3)).^2);
 func3 = tdoa14 - sqrt((x-xyz(1,1)).^2 + (y-xyz(1,2)).^2 + (z-xyz(1,3)).^2) + sqrt((x-xyz(4,1)).^2 + (y-xyz(4,2)).^2 + (z-xyz(4,3)).^2);
-func4 = tdoa15 - sqrt((x-xyz(1,1)).^2 + (y-xyz(1,2)).^2 + (z-xyz(1,3)).^2) + sqrt((x-xyz(5,1)).^2 + (y-xyz(5,2)).^2 + (z-xyz(5,3)).^2);
 
-outF = [func1; func2; func3; func4];
+outF = [func1; func2; func3];
 
 end
 
--- MATLAB/test.m
+++ MATLAB/test.m
@@ -1,13 +1,1 @@
 
-clear all
-close all
-%Reads the data and plots them
-[mic1, f] = audioread('mic1.wav');
-[mic2, f] = audioread('mic2.wav');
-[mic3, f] = audioread('mic3.wav');
-[mic4, f] = audioread('mic4.wav');
-[mic5, f] = audioread('mic5.wav');
-
-plot(mic1)
-figure
-plot(mic5)
