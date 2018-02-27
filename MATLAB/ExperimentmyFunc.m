432ece8ef6ae19163c917eb2e7df8531b8e9ad5e
sofieka <tfy13ska@student.lu.se>
2018-02-27 15:24:00 +0100

    Added experiment files

---
+++ MATLAB/ExperimentmyFunc.m
@@ -0,0 +1,12 @@
+function outF = ExperimentmyFunc(param, tdoa, nbrOfSpeakers, xyz)
+x = param(1);
+y = param(2);
+z = param(3);
+outF = [];
+for i = 2:nbrOfSpeakers
+    func = tdoa{i} - sqrt((x-xyz(1,1)).^2 + (y-xyz(1,2)).^2 + (z-xyz(1,3)).^2) + sqrt((x-xyz(i,1)).^2 + (y-xyz(i,2)).^2 + (z-xyz(i,3)).^2);
+    outF = [outF func];
+end
+
+end
+
---
+++ MATLAB/ExperimentscatterPlot.m
@@ -0,0 +1,17 @@
+function ExperimentscatterPlot(xyzMic, nbrOfSpeakers, xS, yS, zS)
+%scatterPlot Plots the microphones and the sound source.
+%   INPUT
+%       xyzMic - a matrix with the coordinates for the four microphones
+%       xS, yS, zS - the coordinates for the sound source
+
+    micX = xyzMic(:,1); micY = xyzMic(:,2); micZ = xyzMic(:,3);
+    figure;
+    scatter3(micX, micY, micZ,100,'sr','MarkerFaceColor','r')
+    a = [1:nbrOfSpeakers]'; b = num2str(a); c = cellstr(b);
+    dx = 0.2; dy = 0.2; dz = 0.2;
+    text(micX+dx, micY+dy, micZ+dz, c);
+    set(gca,'Xlim',[-1, 3], 'YLim', [-1, 3], 'Zlim', [0, 3])
+    hold on
+    scatter3(xS,yS,zS,'*b','MarkerFaceColor','g')
+
+end
