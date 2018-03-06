87e3ad1c1f3f5546c214aae32fd0ac45df7cade4
Martin Chan <martinch@lnxmartinch.se.axis.com>
2018-03-06 10:32:42 +0100

    newrunSim

--- runSim.sh
+++ runSim.sh
@@ -1,5 +1,5 @@
 cat in99.sh | sshpass -p pass ssh root@172.25.12.99 &
 cat in168.sh | sshpass -p pass ssh root@172.25.12.168 &
-cat in38.sh | sshpass -p pass ssh root@172.25.9.38 &
-cat in250.sh | sshpass -p pass ssh root@172.25.13.250 &
-cat in27.sh | sshpass -p pass ssh root@172.25.9.27 &
+#cat in38.sh | sshpass -p pass ssh root@172.25.9.38 &
+#cat in250.sh | sshpass -p pass ssh root@172.25.13.250 &
+#cat in27.sh | sshpass -p pass ssh root@172.25.9.27 &
