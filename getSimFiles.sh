ef529ce890020defd51d009d8ec45c3b445c1451
Sofie Karlsson <sofieka@lnxsofieka1.se.axis.com>
2018-03-13 14:49:57 +0100

    changed directory

--- getSimFiles.sh
+++ getSimFiles.sh
@@ -1,10 +1,10 @@
-sshpass -p "pass" scp root@172.25.9.27:/tmp/mic1.wav ~/MATLAB/MasterThesis/MATLAB/
-sshpass -p "pass" scp root@172.25.12.99:/tmp/mic2.wav ~/MATLAB/MasterThesis/MATLAB/
-sshpass -p "pass" scp root@172.25.9.38:/tmp/mic3.wav ~/MATLAB/MasterThesis/MATLAB/
-sshpass -p "pass" scp root@172.25.13.250:/tmp/mic4.wav ~/MATLAB/MasterThesis/MATLAB/
-sshpass -p "pass" scp root@172.25.12.168:/tmp/mic5.wav ~/MATLAB/MasterThesis/MATLAB/
-sshpass -p "pass" scp root@172.25.14.27:/tmp/mic6.wav ~/MATLAB/MasterThesis/MATLAB/
-sshpass -p "pass" scp root@172.25.15.12:/tmp/mic7.wav ~/MATLAB/MasterThesis/MATLAB/
+sshpass -p "pass" scp root@172.25.9.27:/tmp/mic1.wav ~/MATLAB/MasterThesis/MATLAB/NCS
+sshpass -p "pass" scp root@172.25.12.99:/tmp/mic2.wav ~/MATLAB/MasterThesis/MATLAB/NCS
+sshpass -p "pass" scp root@172.25.9.38:/tmp/mic3.wav ~/MATLAB/MasterThesis/MATLAB/NCS
+sshpass -p "pass" scp root@172.25.13.250:/tmp/mic4.wav ~/MATLAB/MasterThesis/MATLAB/NCS
+sshpass -p "pass" scp root@172.25.12.168:/tmp/mic5.wav ~/MATLAB/MasterThesis/MATLAB/NCS
+sshpass -p "pass" scp root@172.25.14.27:/tmp/mic6.wav ~/MATLAB/MasterThesis/MATLAB/NCS
+sshpass -p "pass" scp root@172.25.15.12:/tmp/mic7.wav ~/MATLAB/MasterThesis/MATLAB/NCS
 
 
 #sox -r 48000 -e signed-integer -b 16 -c 1 cap168.raw  ~/MATLAB/MasterThesis/MATLAB/cap168.wav
