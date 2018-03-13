372a3c54f6d81424e4e1be20a4ab291fb0096bfc
Sofie Karlsson <sofieka@lnxsofieka1.se.axis.com>
2018-03-13 10:04:21 +0100

    fixed Experiment-files

--- MATLAB/ExperimentCheckSyncQuality.m
+++ MATLAB/ExperimentCheckSyncQuality.m
@@ -7,20 +7,20 @@ for i=2:nbrOfSpeakers
 end
 err = norm(realDist - dist);
 if err > 1
-    quality(1,:) = 'bad';
+    quality{1} = 'bad';
 elseif err < 0.5
-    quality(1,:) = 'good';
+    quality{1} = 'good';
 else
-    quality(1,:) = 'ok';
+    quality{1} = 'ok';
 end
 
 %Check synchronization using checkSync2
 j = 2;
 for i=2:nbrOfSpeakers
     if i == nbrOfSpeakers
-        quality(j,:) = checkSync2(micMatrix{i}, micMatrix{2}, sVec(i), sVec(2))
+        quality{j} = checkSync2(micMatrix{i}, micMatrix{2}, sVec(i), sVec(2));
     else
-        quality(j,:) = checkSync2(micMatrix{i}, micMatrix{i+1}, sVec(i), sVec(i+1))
+        quality{j} = checkSync2(micMatrix{i}, micMatrix{i+1}, sVec(i), sVec(i+1));
     end
     j = j+1;
 end
