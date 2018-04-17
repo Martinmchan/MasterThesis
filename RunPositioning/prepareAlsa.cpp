329950ca659e9790652f5daa1abb1342a735ae2a
Sofie Karlsson <sofieka@lnxsofieka1.se.axis.com>
2018-04-17 09:07:03 +0200

    prepare

--- RunPositioning/prepareAlsa.cpp
+++ RunPositioning/prepareAlsa.cpp
@@ -3,41 +3,47 @@
 #include <fstream>
 #define ERROR(...)	do { fprintf(stderr, "Error: "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); exit(1); } while(0)
 
-using namespace std;
-
-const vector<string> readIPs(){
-	ifstream confFile("./configuration.txt");
-	vector<string> ips;
-	string line;
-	if(confFile.is_open()){
-		getline(confFile, line);
-		getline(confFile, line);
-		int nbrIP = stoi(line);
-		for (int i = 0; i < nbrIP; i++){
-			getline(confFile, line);
-			ips.push_back(line);
-		}
-		
-	}
-	return ips;
-}
+using namespace std;	
 
 int main(int argc, char** argv) {
 	SSHMaster master;
-	vector<string> ips = readIPs();
-	if (!master.connect(ips,"pass"))
-		cout << "Could not connect to speakers\n";
-	
-	if (argc == 0)
-		cout << "Could not start recordning, enter record time"	
-	
-	for (int i = 1; i < ips.size() + 1; i++) {
-		command = "cd /var/spool/storage/SD_DISK; arecord -Daudiosource -fS16_LE -r48000 -d" + to_string(argv[1]) + " mic" + to_string(i) + ".wav; wait;";
-		commands.push_back(command);
-	}
-	master.command(ips, commands, true);
 
-	printSSHOutput(master.command(ips, commands, true));
+	string line;
+	ifstream confFile("./configuration.txt");
+	if (confFile.is_open()){
+		getline(confFile, line);
+		getline(confFile, line);
+		getline(confFile, line);
+		string speakerIP = line;
+
+		string command;
+		vector<string> commands;
+
+		command = "dspd -s -b loudness; dspd -s -b compressor; dspd -s -b preset; dspd -s -b hpf; dspd -s -b vad; dspd -s -b mic; wait;";
+		commands.push_back(command);
+		
+		master.command(speakerIP, commands, true);
+
+		commands.clear();
+		command = "killall audio-netrecv; wait;";
+		commands.push_back(command);
+		
+		master.command(speakerIP, commands, true);
+
+		vector<string> from;
+		vector<string> to;
+
+		string filename = "./ring1mod.wav";
+		from.push_back(filename);
+		to.push_back("/tmp/");
+
+		master.setSetting(SETTING_ENABLE_SSH_OUTPUT_VECTOR_STYLE, true);
 	
-	return 0;
+		master.transferRemote(speakerIP, from, to);
+
+	else
+		cout << "Could not open Configuration.txt"
+
+	return 0	
+		
 }
