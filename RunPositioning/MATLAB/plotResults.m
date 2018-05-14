0cb5b3acd3151aa2998f302a0ef4c8f2fee0004c
Martin Chan <martinch@lnxmartinch.se.axis.com>
2018-05-14 08:54:37 +0200

    plotresults

---
+++ RunPositioning/MATLAB/plotResults.m
@@ -0,0 +1,24 @@
+close all;
+clear all;
+
+fileID = fopen('../configuration.txt');
+confFile = textscan(fileID,'%s', 'delimiter', '\t','collectoutput',true);
+confFile=confFile{1};
+fclose(fileID);
+micMatrix = str2num(confFile{end});
+nbrOfSpeakers = length(micMatrix(:,1));
+lsb = [-1,-1,1];
+usb = [max(micMatrix(:,1)) + 1,max(micMatrix(:,2)) + 1, 2];
+
+
+fileID = fopen('./100ComplexSyncedPos.txt');
+results = textscan(fileID,'%f %f %f %f', 'delimiter', '\t','collectoutput',true);
+results = results{1};
+fclose(fileID);
+
+
+%Plots the results
+numbering = 0;
+ourPlot(micMatrix, nbrOfSpeakers, results(:,1:3), lsb, usb, numbering)
+
+scatter3(5, 1.6, 2,100,'or','MarkerFaceColor','r')
